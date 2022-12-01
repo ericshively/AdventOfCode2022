76739940ac71950eff098a6cec0012568ed1eab5
Eric Shively <eshively@uci.edu>
2022-11-30 22:41:41 -0800

    Add optimal solutions

---
+++ 01/optimal_a.ts
@@ -0,0 +1,15 @@
+import * as p from "https://deno.land/std@0.165.0/path/mod.ts";
+
+const input = await Deno.readTextFile(
+  p.fromFileUrl(import.meta.resolve("./input.txt"))
+);
+
+const totals = input
+  .split("\n\n")
+  .map((elf) => {
+    return elf.split("\n").reduce((acc, val) => acc + parseInt(val), 0);
+  })
+  .sort()
+  .reverse();
+
+console.log(totals[0]);
