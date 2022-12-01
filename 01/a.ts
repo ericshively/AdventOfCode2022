4aa21a64a60fbcb802be843496db0b01856d2b9d
Eric Shively <eshively@uci.edu>
2022-11-30 21:43:48 -0800

    Finished day 1 rough solution

--- 01/a.ts
+++ 01/a.ts
@@ -4,4 +4,43 @@ const input = await Deno.readTextFile(
   p.fromFileUrl(import.meta.resolve("./input.txt"))
 );
 
-console.log(input);
+// delim by new line into array
+// iterate through array adding values to count until delim by blank
+// new array pushes a count
+// iterate through new array finding position of max
+
+const delimByNewline = input.split(/\r?\n/);
+
+let newArrayValues: number[] = [];
+let count = 0;
+delimByNewline.map((calorieVal) => {
+  if (calorieVal === "") {
+    newArrayValues.push(count);
+    count = 0;
+  } else {
+    count += Number(calorieVal);
+  }
+});
+newArrayValues.push(count);
+
+console.log(newArrayValues);
+let newTotal = 0;
+
+let maxVal = Math.max(...newArrayValues);
+newTotal += maxVal;
+let index = newArrayValues.indexOf(maxVal);
+newArrayValues.splice(index, 1);
+console.log(newArrayValues);
+console.log(newTotal);
+
+maxVal = Math.max(...newArrayValues);
+newTotal += maxVal;
+index = newArrayValues.indexOf(maxVal);
+newArrayValues.splice(index, 1);
+console.log(newTotal);
+
+maxVal = Math.max(...newArrayValues);
+newTotal += maxVal;
+index = newArrayValues.indexOf(maxVal);
+newArrayValues.splice(index, 1);
+console.log(newTotal);
